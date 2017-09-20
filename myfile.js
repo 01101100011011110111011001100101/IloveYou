42ca594743971a615f84116cae66339860f63063
01101100011011110111011001100101 <32053499+01101100011011110111011001100101@users.noreply.github.com>
2017-09-20 00:01:57 -0700

    Create myfile.js

---
+++ myfile.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  var j = 0;
+  var delay = 2000; //millisecond delay between cycles
+  function cycleThru() {
+    var jmax = $("div#cyclelist h2").length - 1;
+    $("div#cyclelist h2:eq(" + j + ")")
+      .animate({
+        "opacity": "1"
+      }, 400)
+      .animate({
+        "opacity": "1"
+      }, delay)
+      .animate({
+        "opacity": "0"
+      }, 400, function() {
+        (j == jmax) ? j = 0: j++;
+        cycleThru();
+      });
+  };
+  cycleThru();
+
+});
