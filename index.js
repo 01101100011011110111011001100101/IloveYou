a2fceaacf57370323f5ab793d1b83e75a9d3fbe2
01101100011011110111011001100101 <32053499+01101100011011110111011001100101@users.noreply.github.com>
2017-09-19 22:29:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -141,6 +141,21 @@ var sketch = Sketch.create({
   }
 });
 
-$(document).ready(function () {
-    $('div.hidden').fadeIn(4000).removeClass('hidden');
+$(document).ready(function() {
+
+     var j = 0;
+     var delay = 4000; //millisecond delay between cycles
+     function cycleThru(){
+             var jmax = $("div#cyclelist h2").length -1;
+             $("div#cyclelist h2:eq(" + j + ")")
+                     .animate({"opacity" : "1"} ,400)
+                     .animate({"opacity" : "1"}, delay)
+                     .animate({"opacity" : "0"}, 400, function(){
+                             (j == jmax) ? j=0 : j++;
+                             cycleThru();
+                     });
+             };
+
+     cycleThru();
+
 });
