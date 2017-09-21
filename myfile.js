7c6700e7dcbc6d259e3a0da2db9b2821dbdddfbf
01101100011011110111011001100101 <32053499+01101100011011110111011001100101@users.noreply.github.com>
2017-09-20 17:30:43 -0700

    Update myfile.js

--- myfile.js
+++ myfile.js
@@ -1,9 +1,6 @@
 
 
 $(document).ready(function() {
- $("div#fader2")
-.animate({"opacity":"0"},10000)
-.animate({"opacity":"1"},400)
   var j = 0;
   var delay = 2100; //millisecond delay between cycles
   function cycleThru() {
