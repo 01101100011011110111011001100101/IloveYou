2f06f08639c95fdd399cd052ff3babf489edb56a
01101100011011110111011001100101 <32053499+01101100011011110111011001100101@users.noreply.github.com>
2017-09-20 21:58:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('test'),
   ctx = canvas.getContext('2d'),
   stack = [],
-  w = window.innerWidth+180,
+  w = window.innerWidth,
   h = window.innerHeight-76;
 
 var drawer = function() {
