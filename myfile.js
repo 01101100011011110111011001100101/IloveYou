888b901dfabf0ea3e820b02f6f7848bdb55425af
01101100011011110111011001100101 <32053499+01101100011011110111011001100101@users.noreply.github.com>
2017-09-20 17:28:07 -0700

    Update myfile.js

--- myfile.js
+++ myfile.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var j = 0;
   var delay = 2100; //millisecond delay between cycles
   function cycleThru() {
-    $("div#fade2")
+    $("div#fader2")
 .animate({"opacity":"0"},10000)
 .animate({"opacity":"1"},400)
     var jmax = $("div#cyclelist h2").length - 1;
