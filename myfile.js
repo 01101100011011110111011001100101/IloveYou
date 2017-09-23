b56efdf2bee438d3e98778c5e30524247c10472c
01101100011011110111011001100101 <32053499+01101100011011110111011001100101@users.noreply.github.com>
2017-09-22 18:43:12 -0700

    Update myfile.js

--- myfile.js
+++ myfile.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
   var j = 0;
-  var delay = 2000; //millisecond delay between cycles
+  var delay = 3550; //millisecond delay between cycles
   function cycleThru() {
     var jmax = $("div#cyclelist h2").length - 1;
     $("div#cyclelist h2:eq(" + j + ")")
       .animate({
         "opacity": "1"
-      }, 450)
+      }, 400)
       .animate({
         "opacity": "1"
       }, delay)
       .animate({
         "opacity": "0"
-      }, 450, function() {
+      }, 400, function() {
         (j == jmax) ? j = 0: j++;
         cycleThru();
       });
